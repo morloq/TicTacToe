78ddc8834757d607323859095f4aab624c2cb22b
Sara <sarahoubertz@gmail.com>
2022-07-22 11:52:56 +0200

    Change logic

--- javaScript/doStuff.js
+++ javaScript/doStuff.js
@@ -1,48 +1,31 @@
 //create and manage gameboard
 let GameBoard = (function () {
-    let board = [9];
 
-    //get all 9 tiles:
-    const tile0 = document.querySelector(".tile.zero");
-    const tile1 = document.querySelector(".tile.one");
-    const tile2 = document.querySelector(".tile.two");
-    const tile3 = document.querySelector(".tile.three");
-    const tile4 = document.querySelector(".tile.four");
-    const tile5 = document.querySelector(".tile.five");
-    const tile6 = document.querySelector(".tile.six");
-    const tile7 = document.querySelector(".tile.seven");
-    const tile8 = document.querySelector(".tile.eight");
-
-    init();
-
-    function init() {//set tiles to array positions
-        board.push(tile0);
-        board.push(tile1);
-        board.push(tile2);
-        board.push(tile3);
-        board.push(tile4);
-        board.push(tile5);
-        board.push(tile6);
-        board.push(tile7);
-        board.push(tile8);
+    //represents board on website
+    let board = [['0', '1', '2'],['3', '4', '5'],['6', '7', '8']];
+    /*         col     col     col
+      row  0   0,0     1,0     2,0       
+      row  1   0,1     1,1     2,1
+      row  2   0,2     1,2     2,2
+    */
+    function getboard() {
+        return board;
     }
 
-    //add click events to tiles.
-    function listeners() {
-        board.forEach(function(tile) {
-            tile.addEventListener("click", addSymbol());
-        });
+    function setboard(row, column, value) {
+        board[row, column] = value;
     }
 
-    listeners();
-
-    function addSymbol() {
-        alert("clicked");
+    function resetboard() {
+        board = [['0', '1', '2'],['3', '4', '5'],['6', '7', '8']];
     }
 
     return {
+        getboard: getboard(),
+        setboard: setboard(),
+        resetboard: resetboard(),
+    };
 
-    }
 })();
 
 //create and manage players
@@ -62,4 +45,18 @@ let players = (function () {
 //manage gameflow
 let displayControl = (function () {
     
+    //get all 9 tiles:
+    const tile0 = document.querySelector(".tile.zero").addEventListener("click", addSymbolToTile());
+    const tile1 = document.querySelector(".tile.one").addEventListener("click", addSymbolToTile());
+    const tile2 = document.querySelector(".tile.two").addEventListener("click", addSymbolToTile());
+    const tile3 = document.querySelector(".tile.three").addEventListener("click", addSymbolToTile());
+    const tile4 = document.querySelector(".tile.four").addEventListener("click", addSymbolToTile());
+    const tile5 = document.querySelector(".tile.five").addEventListener("click", addSymbolToTile());
+    const tile6 = document.querySelector(".tile.six").addEventListener("click", addSymbolToTile());
+    const tile7 = document.querySelector(".tile.seven").addEventListener("click", addSymbolToTile());
+    const tile8 = document.querySelector(".tile.eight").addEventListener("click", addSymbolToTile());
+
+
+
+
 })();
